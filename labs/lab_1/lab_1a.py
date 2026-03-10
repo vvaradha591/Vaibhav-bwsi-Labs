d63b507e4cee20d701aefd87920f4ccbbc132aaa
vvaradha591 <vaibhavvaradha@gmail.com>
2026-03-10 18:42:08 -0400

    Update lab_1a.py
    Changed speed of robot from 5 to 3

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,5 +1,5 @@
 """
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
+This is to simulate a change made on a robot: robot_speed = 3 # m/s
 lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
