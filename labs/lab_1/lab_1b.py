def56c711ce853e3293cbe6bb6c7377fa2421519
vvaradha591 <vaibhavvaradha@gmail.com>
2026-03-14 18:34:09 -0400

    Fixed Issue with Calculator

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,14 +37,30 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_santized_number(prompt: str) -> float:
+    
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Input invalid. Please enter a valid number.")
+
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    num1 = request_santized_number("Enter the first number: ")
+    num2 = request_santized_number("Enter the second number: ")
+    
+    while True:
+        operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+        if operation in ["add", "subtract", "multiply", "divide"]:
+            break
+        else:
+            print("Invalid operation. Please enter add, subtract, multiply, or divide.")
+
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
